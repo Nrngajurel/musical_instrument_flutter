cae3837c4c6928e9c81b6848414573191f8925ec
shanand <shanand.codehimalaya@gmail.com>
2022-04-12 19:49:19 +0545

    signup validation added

--- lib/pages/signup/components/signup_auth-provider.dart
+++ lib/pages/signup/components/signup_auth-provider.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
 class SignupAuthProvider with ChangeNotifier {
-  static Pattern pattern =
+  static Pattern emailPattern =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
-  RegExp regExp = RegExp(SignupAuthProvider.pattern.toString());
+  RegExp emailRegExp = RegExp(SignupAuthProvider.emailPattern.toString());
+
+  static Pattern numPattern = r'^(?:[+0]9)?[0-9]{10}$';
+  RegExp numRegExp = RegExp(SignupAuthProvider.numPattern.toString());
 
   void signupValidation(
       {required TextEditingController? fullName,
@@ -26,13 +29,43 @@ class SignupAuthProvider with ChangeNotifier {
         ),
       );
       return;
-    } else if (!regExp.hasMatch(email.text.toString())) {
+    } else if (!emailRegExp.hasMatch(email.text.toString())) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text("Invalid email address"),
         ),
       );
       return;
-    }else if()
+    } else if (password!.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Password is empty!!"),
+        ),
+      );
+    } else if (password.text.length < 8) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Password must be more than or equal to 8 characters"),
+        ),
+      );
+    } else if (address!.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Address bar is empty"),
+        ),
+      );
+    } else if (number!.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Number is empty!!"),
+        ),
+      );
+    } else if (!numRegExp.hasMatch(number.text.toString())) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Invalid number"),
+        ),
+      );
+    }
   }
 }
